920400c1fc50db2394a4db1f8b03f4ae111fb33a
gongjt057 <1379734369@qq.com>
2023-04-11 15:21:48 +0800

    THPLM

--- README.md
+++ README.md
@@ -9,32 +9,34 @@ A sequence-based deep learning framework for predicting protein stability change
   - [Table of Contents](#table-of-contents)
   - [Installation](#installation)
     - [Dependencies](#dependencies)
-    - [Computing environment](#computing-environment)
+    - [ESM pretrain model download](#ESM pretrain model download)
+    - [Installation](#Installation)
   - [Usage](#usage)
     - [For prediction](#for-prediction)
     - [Training your own model](#training-your-own-model)
+  - [Docker](#Docker)
+  - [Webserver and tutorial](#Webserver)
   - [Citation](#citation)
 
 ## Installation
 
 ### Dependencies
 
+```bash
+conda env create -f environment.yml
+```
+### ESM pretrain model download
 
-### Computing environment
+The pretain model used in THPLM is [**esm2_t36_3B_UR50D**](https://dl.fbaipublicfiles.com/fair-esm/models/esm2_t36_3B_UR50), it automatically downloaded to `~/.cache/torch/hub/checkpoints` based on the tutorial of [ESM](https://github.com/facebookresearch/esm).
+
+### Installation
 
 If the dependencies are satified, you can install the THPLM using following commands:
 
 ```bash
-conda create -n THPLM_env python=3.xx -y
-conda activate create THPLM_env
-
-pip install esm
-pip install XXX
-
-git clone XXX
-
-cd XXX
-python XXX.py test ## test the code
+conda activate THPLM
+git clone https://github.com/FPPGroup/THPLM.git
+cd THPLM
 ```
 
 ## Usage
@@ -42,8 +44,41 @@ python XXX.py test ## test the code
 ### For prediction
 
 ```bash
+python THPLM_predict.py -h ## show the parameters
+```
+paramaters:
 
+```bash
+usage: THPLM_predict.py [-h] [--model_location MODEL_LOCATION]
+                        [--gpunumber GPUNUMBER]
+                        [--toks_per_batch TOKS_PER_BATCH] [--THPLM THPLM]
+                        [--extractfile EXTRACTFILE] [--pythonbin PYTHONBIN]
+                        variants_file fasta_file output_dir variants_fasta_dir
 
+Extract mean representations and model outputs for sequences in a FASTA file
+and to predict DDGs
+
+positional arguments:
+  variants_file         files inclusing variants, format is
+                        <wildtype><position><mutation> (see README for models)
+  fasta_file            FASTA file on which to extract representations
+  output_dir            output directory for extracted representations
+  variants_fasta_dir    FASTA file stored to get representations
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --model_location MODEL_LOCATION
+                        PyTorch model file OR name of pretrained model to
+                        download (see README for models)
+  --gpunumber GPUNUMBER
+                        GPU number for use
+  --toks_per_batch TOKS_PER_BATCH
+                        maximum batch size
+  --THPLM THPLM         maximum batch size
+  --extractfile EXTRACTFILE
+                        the path of extract.py file from esm-2
+  --pythonbin PYTHONBIN
+                        the path of python bin
 ```
 
 ### Training your own model
@@ -52,10 +87,13 @@ To train your own model, you need to build your fasta file firstly. And then you
 
 ```bash
 
+python THPLM_predict.py ./examples/var.txt ./examples/wild.fasta ./examples/esm3Bout/ ./examples/varlist.fasta --gpunumber 0 --extractfile ./esmscripts/extract.py
 
 ```
+## Docker
 
+## Webserver and tutorial
 
 ## Citation
 
-hollo
+
---
+++ THPLM_predict.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+
+import argparse
+import pathlib
+from torch.utils.data import DataLoader
+import torch,sys,os
+import numpy as np
+import pandas as pd
+from esm import Alphabet, FastaBatchedDataset, ProteinBertModel, pretrained, MSATransformer
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description="Extract mean representations and model outputs for sequences in a FASTA file and to predict DDGs"  # noqa
+    )
+
+    parser.add_argument(
+        "variants_file",
+        type=pathlib.Path,
+        default='./examples/var.txt',
+        help="files inclusing variants, format is <wildtype><position><mutation> (see README for models)",
+    )
+    parser.add_argument(
+        "--model_location",
+        type=str,
+        help="PyTorch model file OR name of pretrained model to download (see README for models)",
+    )
+    parser.add_argument(
+        "fasta_file",
+        type=pathlib.Path,
+        default='./examples/wild.fasta',
+        help="FASTA file on which to extract representations",
+    )
+    parser.add_argument(
+        "output_dir",
+        type=pathlib.Path,
+        default="./examples/esm3Bout/",
+        help="output directory for extracted representations",
+    )
+    parser.add_argument(
+        "variants_fasta_dir",
+        type=pathlib.Path,
+        default='./examples/varlist.fasta',
+        help="FASTA file stored to get representations",
+    )
+    parser.add_argument(
+        "--gpunumber",
+        type=int,
+        default=0,
+        help="GPU number for use",
+    )
+    parser.add_argument("--toks_per_batch", type=int, default=4096, help="maximum batch size")
+    parser.add_argument("--THPLM", type=pathlib.Path, default="./Model/S4596_1832_2CNN.pt", help="maximum batch size")
+    parser.add_argument("--extractfile", type=pathlib.Path, default='./esmscripts/extract.py', help="the path of extract.py file from esm-2")
+    parser.add_argument(
+        "--pythonbin",
+        type=str,
+        default='python',
+        help="the path of python bin",
+    )
+    #parser.add_argument("--nogpu", action="store_true", help="Do not use GPU even if available")
+    return parser
+
+class MyCNNNet(torch.nn.Module):
+    def __init__(self):
+        super(MyCNNNet, self).__init__()
+        self.cnn_layers = torch.nn.Sequential(
+
+            torch.nn.Conv1d(1, 8, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(8),
+            #torch.nn.MaxPool1d(2),
+
+            torch.nn.Conv1d(8, 32, 3, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.BatchNorm1d(32),
+            #torch.nn.MaxPool1d(2),
+
+        )
+
+
+        self.age_fc_layers = torch.nn.Sequential(
+            torch.nn.Linear(2560*32, 128),
+            torch.nn.BatchNorm1d(128),
+            torch.nn.Linear(128, 1),
+        )
+
+    def forward(self, x):
+        x = self.cnn_layers(x)
+        x = x.view(x.size()[0], -1)
+        #print(x.shape)
+        x= self.age_fc_layers(x)
+        return x
+
+
+def parser_rep(args):
+
+    #identify file or path
+
+    #parser input
+    #parser variant list
+
+    varlist = []
+    try:
+        with open(args.variants_file,'r') as varfile:
+            for eachline in varfile:
+                varlist.append(eachline.strip('\n'))
+    except FileNotFoundError as e:
+        print('variants file does not avaliable!')
+    except IOError as e:
+        print('The file can not open!')
+
+    #parser wildtype protein
+    variantsfile = open(args.variants_fasta_dir,'w')
+    seq = ''
+    with open(args.fasta_file) as variant_fasta:
+        for varline in variant_fasta:
+            if len(varline.strip('\n')) != 0:
+                if varline[0] != '>':
+                    seq += varline.strip('\n')
+                else:
+                    IDs = varline.strip('\n')[1:]
+                    variantsfile.write(varline)
+        variantsfile.write(seq +'\n')
+
+    #parser to sequences and check
+    varrep = []
+    for varwpm in varlist:
+        wildAA = varwpm[0]
+        mutaAA = varwpm[-1]
+        posi = int(varwpm[1:-1])
+        if seq[posi-1] == wildAA:
+            newseq = seq[:posi-1] + mutaAA + seq[posi:]
+            variantsfile.write('>' + IDs + '_' + varwpm + '\n')
+            varrep.append(IDs + '_' + varwpm)
+            variantsfile.write(newseq + '\n')
+            newseq=''
+        else:
+            print('The mutation {} is wrong, Please input right mutation!'.format(varwpm))
+            sys.exit(0)
+#/public/data1/gongj/esm/scripts/extract.py
+    #run esm-2
+    cmd = 'CUDA_VISIBLE_DEVICES={} time {} {} esm2_t36_3B_UR50D {}  {}/ --repr_layers 36 --include mean --toks_per_batch {}'.format(args.gpunumber,args.pythonbin,args.extractfile,args.variants_fasta_dir,args.output_dir,args.toks_per_batch)
+    print(cmd)
+    #os.system(cmd)
+    ######### repe extract ############
+    queryproteinrep = []
+    for eachvar in varrep:
+        rep_changes = torch.load(os.path.join(args.output_dir,eachvar+'.pt'))['mean_representations'][36]-torch.load(os.path.join(args.output_dir,eachvar.split('_')[0]+'.pt'))['mean_representations'][36]
+        queryproteinrep.append(rep_changes.tolist())
+
+    return torch.Tensor(queryproteinrep).unsqueeze(1),varrep
+
+def main(args):
+    query_rep,varrep = parser_rep(args)
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(args.gpunumber)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('the device is %s' % device)
+
+    model = MyCNNNet().to(device)
+    model.load_state_dict(torch.load(args.THPLM))
+    model.eval()
+    output = model(query_rep.to(device))
+    output = output.cpu().detach().numpy().reshape(-1, )
+    DDG = {}
+    for eachi in range(len(varrep)):
+        DDG.update({varrep[eachi]:output[eachi]})
+    return DDG
+
+if __name__ == "__main__":
+    parser = create_parser()
+    args = parser.parse_args()
+    output = main(args)
+    print(output)
